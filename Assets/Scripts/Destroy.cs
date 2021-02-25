2a528a2aeca9d8c66ccc5b302f6d64b1e9e69acd
emkidd <ekidd@csumb.edu>
2021-02-25 14:56:55 -0800

    Question blocks increase coin count when destroyed

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Destroy : MonoBehaviour
 {
     public float distance;
     public LayerMask layermask;
+
     public GameObject coinText;
-    private int coinCount;
+    private int coinCount = 00;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -18,8 +21,22 @@ public class Destroy : MonoBehaviour
             if (Physics.Raycast(ray, out hitInfo, distance, layermask))
             {
                 Destroy(hitInfo.transform.gameObject);
+
+                if (hitInfo.collider.name == "QuestionBlock(Clone)")
+                {
+                    coinCount++;
+
+                    if (coinCount < 10)
+                    {
+                        coinText.GetComponent<Text>().text = "0" + coinCount;
+                    }
+                    else
+                    {
+                        coinText.GetComponent<Text>().text = coinCount.ToString();
+                    }
+                }
             }
+            Debug.Log(coinCount);
         }
-        
     }
 }
